86d06f4af34b106930ff2129a98a5184cbad1419
Ewan Pinnington <ewan.pinnington@gmail.com>
2015-02-19 15:20:49 +0000

    changed plot

--- src/model/plot.py
+++ src/model/plot.py
@@ -147,7 +147,7 @@ def plotscatterobs(ob, pvals, dC, awindl, bfa='a'):
                                                             len(y[splitval:]))
     else:
         raise Exception('Please check function input for bfa variable')
-    return error
+    return error, y[0:splitval]-hx[0:splitval]
 
     
 def plotcycled4dvar(ob, conditions, xb, xa, dC, erbars=1, truth=None):
