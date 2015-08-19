484bcf7889bff0339a6224f538b41b963d42ee98
Ewan Pinnington <ewan.pinnington@gmail.com>
2015-08-19 12:09:34 +0100

    corrected r matrices

--- src/model/plot.py
+++ src/model/plot.py
@@ -455,13 +455,13 @@ def plotbmat(bmat):
 #            square=True, xticklabels=5, yticklabels=5,
 #            linewidths=.5, cbar_kws={"shrink": .5}, ax=ax)
 
-def plotrmat(rmat, ticks=5):
+def plotrmat(rmat):
     """Plots a R matrix.
     """
     sns.set(style="white")
     fig, ax = plt.subplots(figsize=(11,9))
     ax.set_aspect('equal')
-    sns.heatmap(rmat, ax=ax, xticklabels=ticks, yticklabels=ticks)
+    sns.heatmap(rmat, ax=ax, xticklabels=len(rmat), yticklabels=len(rmat))
     return ax, fig
     
 def analysischange(xb, xa):
