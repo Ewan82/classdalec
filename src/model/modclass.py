fe902bcee46e9540f598a60b05656f398d7225b3
Ewan Pinnington <ewan.pinnington@gmail.com>
2015-08-20 11:10:49 +0100

    added lbfgsb

--- src/model/modclass.py
+++ src/model/modclass.py
@@ -742,7 +742,7 @@ class DalecModel():
                                          'ftol': factr})
         return findmin
 
-    def findminlbfgsb(self, pvals, bnds='strict', factr=1e5, prnt=-1, dispp=5):
+    def findminlbfgsb(self, pvals, bnds='strict', factr=1e5, prnt=-1, dispp=5, mm=23):
         """Function which minimizes 4DVAR cost fn. Takes an initial state
         (pvals).
         """
@@ -752,7 +752,7 @@ class DalecModel():
             bnds = bnds
         findmin = spop.fmin_l_bfgs_b(self.cost, pvals,
                                      fprime=self.gradcost, bounds=bnds,
-                                     iprint=prnt, factr=factr, disp=dispp)
+                                     iprint=prnt, factr=factr, disp=dispp, m=mm)
         return findmin             
 
     def findmintnc(self, pvals, bnds='strict', dispp=None, maxits=2000, 
